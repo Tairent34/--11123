038f3e75fcd7c29902eec79878a57203f272a0a4
Tairent34 <d.a.monov@yandex.ru>
2026-01-01 17:38:53 +0300

    Первоначальные файлы для проекта MakeCode

---
+++ main.py
@@ -0,0 +1,143 @@
+def on_mob_killed_sheep():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(SHEEP, on_mob_killed_sheep)
+
+def on_mob_killed_ocelot():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(OCELOT, on_mob_killed_ocelot)
+
+def on_mob_killed_horse():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(HORSE, on_mob_killed_horse)
+
+def on_mob_killed_sniffer():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(SNIFFER, on_mob_killed_sniffer)
+
+def on_mob_killed_axolotl():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(AXOLOTL, on_mob_killed_axolotl)
+
+def on_mob_killed_strider():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(STRIDER, on_mob_killed_strider)
+
+def on_mob_killed_dolphin():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(DOLPHIN, on_mob_killed_dolphin)
+
+def on_mob_killed_rabbit():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(RABBIT, on_mob_killed_rabbit)
+
+def on_mob_killed_armadillo():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(ARMADILLO, on_mob_killed_armadillo)
+
+def on_mob_killed_chicken():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(CHICKEN, on_mob_killed_chicken)
+
+def on_mob_killed_camel():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(CAMEL, on_mob_killed_camel)
+
+def on_mob_killed_squid():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(SQUID, on_mob_killed_squid)
+
+def on_mob_killed_panda():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(PANDA, on_mob_killed_panda)
+
+def on_mob_killed_happy_ghast():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(HAPPY_GHAST, on_mob_killed_happy_ghast)
+
+def on_mob_killed_tropical_fish():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(TROPICAL_FISH, on_mob_killed_tropical_fish)
+
+def on_on_chat():
+    pass
+player.on_chat("run", on_on_chat)
+
+def on_mob_killed_fox():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(FOX, on_mob_killed_fox)
+
+def on_mob_killed_zombie_horse():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(ZOMBIE_HORSE, on_mob_killed_zombie_horse)
+
+def on_mob_killed_donkey():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(DONKEY, on_mob_killed_donkey)
+
+def on_mob_killed_salmon():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(SALMON, on_mob_killed_salmon)
+
+def on_mob_killed_mushroom_cow():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(MUSHROOM_COW, on_mob_killed_mushroom_cow)
+
+def on_mob_killed_frog():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(FROG, on_mob_killed_frog)
+
+def on_mob_killed_glow_squid():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(GLOW_SQUID, on_mob_killed_glow_squid)
+
+def on_mob_killed_sea_turtle():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(SEA_TURTLE, on_mob_killed_sea_turtle)
+
+def on_mob_killed_wandering_trader():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(WANDERING_TRADER, on_mob_killed_wandering_trader)
+
+def on_mob_killed_parrot():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(PARROT, on_mob_killed_parrot)
+
+def on_mob_killed_tadpole():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(TADPOLE, on_mob_killed_tadpole)
+
+def on_mob_killed_bat():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(BAT, on_mob_killed_bat)
+
+def on_mob_killed_cow():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(COW, on_mob_killed_cow)
+
+def on_mob_killed_cat():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(CAT, on_mob_killed_cat)
+
+def on_mob_killed_mule():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(MULE, on_mob_killed_mule)
+
+def on_mob_killed_allay():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(ALLAY, on_mob_killed_allay)
+
+def on_mob_killed_pig():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(PIG, on_mob_killed_pig)
+
+def on_mob_killed_skeleton_horse():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(SKELETON_HORSE, on_mob_killed_skeleton_horse)
+
+def on_mob_killed_cod():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(COD, on_mob_killed_cod)
+
+def on_mob_killed_villager():
+    mobs.apply_effect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+mobs.on_mob_killed(VILLAGER, on_mob_killed_villager)
---
+++ main.ts
@@ -0,0 +1,108 @@
+mobs.onMobKilled(SHEEP, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(OCELOT, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(HORSE, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(SNIFFER, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(AXOLOTL, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(STRIDER, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(DOLPHIN, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(RABBIT, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(ARMADILLO, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(CHICKEN, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(CAMEL, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(SQUID, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(PANDA, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(HAPPY_GHAST, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(TROPICAL_FISH, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+player.onChat("run", function () {
+	
+})
+mobs.onMobKilled(FOX, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(ZOMBIE_HORSE, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(DONKEY, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(SALMON, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(MUSHROOM_COW, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(FROG, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(GLOW_SQUID, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(SEA_TURTLE, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(WANDERING_TRADER, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(PARROT, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(TADPOLE, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(BAT, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(COW, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(CAT, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(MULE, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(ALLAY, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(PIG, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(SKELETON_HORSE, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(COD, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
+mobs.onMobKilled(VILLAGER, function () {
+    mobs.applyEffect(WITHER, mobs.target(LOCAL_PLAYER), 800, 1)
+})
